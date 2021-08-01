17782b043d06f6f28128a244fc07af9949fb2cbd
Wolfy <63961377+Wolfy024@users.noreply.github.com>
2021-08-01 22:24:11 +0530

    Update breh.py

--- RenaBot/breh.py
+++ RenaBot/breh.py
@@ -58,7 +58,7 @@ async def download_with_progressbar(client, msg,down_location,edited):
         )
     return download_location
 
-async def upload_with_progress_bar(client, reply, file_location, name=None, thumbnail=None,edited):
+async def upload_with_progress_bar(client, file_location, name=None, thumbnail=None,edited):
     timer = Timer()
     if name == None:
         name = file_location.split("/")[-1]
