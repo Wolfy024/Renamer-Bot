593b67692476938152b8f1b58a3821e7d4aeda43
Wolfy <63961377+Wolfy024@users.noreply.github.com>
2022-04-11 21:17:12 +0530

    Update renamer.py

--- RenaBot/renamer.py
+++ RenaBot/renamer.py
@@ -169,6 +169,7 @@ async def renamer(event):
 async def batchrenamer(event):
     chatwhere=event.chat_id
     global usage
+    print(1)
     usage=True
     reply=await event.get_reply_message()
     text=event.raw_text
@@ -178,6 +179,7 @@ async def batchrenamer(event):
         tasks.clear()
         return
     text=text.split(" ",1)
+    print(2)
     try:
         _=text[1]
     except:
@@ -186,11 +188,14 @@ async def batchrenamer(event):
         tasks.clear()
         return
     Amount_Fetcher=int(*re.findall(r'\d+', text[0]))
+    print(3)
     if Amount_Fetcher==0:
         Batch=False
     else:
         Batch=True
     temp=text[1]
+    print(4)
+    print(temp)
     if '#zzz' in temp:
         number=temp.split("#zzz",1)
         file_name=number[0]
